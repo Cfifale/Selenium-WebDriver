9e53516ee1c5d7b1346558f888ce2779da9e8324
cfifale <cfifale@yandex.ru>
2020-02-27 02:37:57 +0300

    Задание №7

--- java-example/src/test/java/ru/stqa/training/selenium/LiteCart.java
+++ java-example/src/test/java/ru/stqa/training/selenium/LiteCart.java
@@ -5,9 +5,14 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertTrue;
 
 public class LiteCart {
     private WebDriver driver;
@@ -16,16 +21,37 @@ public class LiteCart {
     public void start() {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    }
-
-    @Test
-    public void admin() {
         driver.get("http://localhost/litecart/admin/");
         driver.findElement(By.xpath("//input[@name='username']")).sendKeys("admin");
         driver.findElement(By.xpath("//input[@name='password']")).sendKeys("admin");
         driver.findElement(By.xpath("//button[@name='login']")).click();
-        driver.findElement(By.xpath("//span[contains(text(),'Catalog')]")).click();
-        driver.findElement(By.xpath("//i[@class='fa fa-sign-out fa-lg']")).click();
+    }
+
+    @Test
+    public void admin() {
+        ArrayList<String> menu = listElement(By.cssSelector("li#app-"), By.className("name"));
+        for (String s : menu) {
+            driver.findElement(By.linkText(s)).click();
+            assertTrue(areElementsPresent(By.cssSelector("h1")));
+            ArrayList<String> podmenu = listElement(By.cssSelector("ul.docs > li"), By.className("name"));
+            for (String s2 : podmenu) {
+                driver.findElement(By.linkText(s2)).click();
+                assertTrue(areElementsPresent(By.cssSelector("h1")));
+            }
+        }
+    }
+
+    private ArrayList<String> listElement(By forma, By elementForms) {
+        List<WebElement> elements = driver.findElements(forma);
+        ArrayList<String> list = new ArrayList<>();
+        for (WebElement element: elements) {
+            list.add(element.findElement(elementForms).getText());
+        }
+        return list;
+    }
+
+    private boolean areElementsPresent(By locator) {
+        return driver.findElements(locator).size() > 0;
     }
 
     @After
