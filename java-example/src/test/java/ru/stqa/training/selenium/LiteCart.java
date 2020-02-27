5d1a161eda3dc0fc239bad1dcecd95734812c649
cfifale <cfifale@yandex.ru>
2020-02-28 01:37:25 +0300

    Задание №8

--- java-example/src/test/java/ru/stqa/training/selenium/LiteCart.java
+++ java-example/src/test/java/ru/stqa/training/selenium/LiteCart.java
@@ -4,7 +4,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -15,20 +14,20 @@ import java.util.concurrent.TimeUnit;
 import static org.junit.Assert.assertTrue;
 
 public class LiteCart {
-    private WebDriver driver;
+    private ChromeDriver driver;
 
     @Before
     public void start() {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("http://localhost/litecart/admin/");
-        driver.findElement(By.xpath("//input[@name='username']")).sendKeys("admin");
-        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("admin");
-        driver.findElement(By.xpath("//button[@name='login']")).click();
     }
 
     @Test
     public void admin() {
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.xpath("//input[@name='username']")).sendKeys("admin");
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("admin");
+        driver.findElement(By.xpath("//button[@name='login']")).click();
         ArrayList<String> menu = listElement(By.cssSelector("li#app-"), By.className("name"));
         for (String s : menu) {
             driver.findElement(By.linkText(s)).click();
@@ -38,6 +37,22 @@ public class LiteCart {
                 driver.findElement(By.linkText(s2)).click();
                 assertTrue(areElementsPresent(By.cssSelector("h1")));
             }
+        }
+    }
+
+    @Test
+    public void testProductSticker() {
+        driver.get("http://localhost/litecart/en/");
+        stickerCheck(By.id("box-most-popular"));
+        stickerCheck(By.id("box-campaigns"));
+        stickerCheck(By.id("box-latest-products"));
+    }
+
+    private void  stickerCheck (By productCategory) {
+        WebElement category = driver.findElement(productCategory);
+        List<WebElement> products = category.findElements(By.cssSelector("li"));
+        for (WebElement element: products) {
+            assertTrue(element.findElements(By.cssSelector("div.image-wrapper > div")).size() == 1);
         }
     }
 
